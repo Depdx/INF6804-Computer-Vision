a1f4da390ee3ace480f7d97964532d4e36502888
Nicolas Depelteau <nicolas@depelteau.ca>
2024-01-17 13:32:45 -0500

    Add Database class and get_images method

--- TP1/main.py
+++ TP1/main.py
@@ -1,1 +1,6 @@
-import ipykernel
+from src.database import Database
+
+db = Database(directory="./data/database")
+
+images = db.get_images()
+print(next(images))
---
+++ TP1/src/database.py
@@ -0,0 +1,43 @@
+import os
+import skimage
+import functools
+
+
+class Database:
+    def __init__(self, directory: str):
+        self.directory = directory
+        self.files = os.listdir(directory)
+        self.feature_extractor = None
+        self.descriptor = None
+
+    def set_feature_extractor(self, feature_extractor: callable, descriptor: callable):
+        self.feature_extractor = feature_extractor
+        self.descriptor = descriptor
+
+    def query(self, query, similarity_measure: callable, k: int):
+        images_features = self.get_images_features()
+        query_features = self.descriptor(self.feature_extractor(query))
+        similarities = [
+            similarity_measure(query_features, image_features)
+            for image_features in images_features
+        ]
+
+        return sorted(
+            zip(similarities, self.get_images(), images_features),
+            key=lambda x: x[0],
+            reverse=True,
+        )[:k]
+
+    @functools.lru_cache(maxsize=128)
+    def get_images_features(self):
+        assert self.feature_extractor is not None
+        assert self.descriptor is not None
+        images_features = []
+        for file in self.files:
+            image = skimage.io.imread(f"{self.directory}/{file}")
+            images_features.append(self.descriptor(self.feature_extractor(image)))
+        return images_features
+
+    def get_images(self):
+        for file in self.files:
+            yield skimage.io.imread(f"{self.directory}/{file}")
