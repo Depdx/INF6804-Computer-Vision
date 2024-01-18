a2dac946dd5f48034d4cfc9850a1698264761724
Nicolas Depelteau <nicolas@depelteau.ca>
2024-01-17 19:34:13 -0500

    added code documentation

--- TP1/src/database.py
+++ TP1/src/database.py
@@ -1,9 +1,27 @@
-import os
-import skimage
-import functools
+"""
+A module for representing a database of images for similarity search.
+"""
+from functools import lru_cache
+from os import listdir
+from skimage.io import imread
 
 
 class Database:
+    """
+    A class representing a database of images for similarity search.
+
+    Args:
+        directory (str): The directory path where the images are stored.
+        feature_extractor (callable): A function or method used to extract features from an image.
+        descriptor (callable): A function or method used to compute descriptors from image features.
+
+    Attributes:
+        directory (str): The directory path where the images are stored.
+        files (list): A list of file names in the directory.
+        feature_extractor (callable): A function or method used to extract features from an image.
+        descriptor (callable): A function or method used to compute descriptors from image features.
+    """
+
     def __init__(
         self,
         directory: str,
@@ -11,11 +29,24 @@ class Database:
         descriptor: callable,
     ):
         self.directory = directory
-        self.files = os.listdir(directory)
+        self.files = listdir(directory)
         self.feature_extractor = feature_extractor
         self.descriptor = descriptor
 
     def query(self, query, similarity_measure: callable, k: int):
+        """
+        Perform a similarity search on the database using a query image.
+
+        Args:
+            query: The query image.
+            similarity_measure (callable): A function or method used to measure similarity
+                between images.
+            k (int): The number of most similar images to retrieve.
+
+        Returns:
+            list: A list of tuples containing the
+                similarity score, image, image features, and file name.
+        """
         images_features = self.get_images_features()
         query_features = self.descriptor(self.feature_extractor(query))
         similarities = [
@@ -29,16 +60,28 @@ class Database:
             reverse=True,
         )[:k]
 
-    @functools.lru_cache(maxsize=128)
+    @lru_cache(maxsize=128)
     def get_images_features(self):
+        """
+        Get the features of all images in the database.
+
+        Returns:
+            list: A list of image features.
+        """
         assert self.feature_extractor is not None
         assert self.descriptor is not None
         images_features = []
         for file in self.files:
-            image = skimage.io.imread(f"{self.directory}/{file}")
+            image = imread(f"{self.directory}/{file}")
             images_features.append(self.descriptor(self.feature_extractor(image)))
         return images_features
 
     def get_images(self):
+        """
+        Generator function to yield images from the database.
+
+        Yields:
+            ndarray: An image from the database.
+        """
         for file in self.files:
-            yield skimage.io.imread(f"{self.directory}/{file}")
+            yield imread(f"{self.directory}/{file}")
--- TP1/src/descriptors/histogram.py
+++ TP1/src/descriptors/histogram.py
@@ -1,15 +1,39 @@
-import numpy as np
+"""
+Histogram descriptor.
+"""
+from numpy import ndarray, arange, histogram
 
 
 class Histogram:
+    """
+    Computes the histogram of a given set of features.
+    """
+
     def __init__(
         self,
-        bins=np.arange(0, 10),
+        bins=arange(0, 10),
         density=True,
     ):
+        """
+        Initializes a Histogram object.
+
+        Args:
+            bins (np.ndarray): The bin edges for the histogram.
+            density (bool): If True, the histogram is normalized to form a probability density.
+        """
         self.bins = bins
         self.density = density
 
-    def __call__(self, features: np.ndarray):
-        hist, _ = np.histogram(features, bins=self.bins, density=self.density)
+    def __call__(self, features: ndarray):
+        """
+        Computes the histogram of the given features.
+
+        Args:
+            features (np.ndarray): The input features.
+
+        Returns:
+            np.ndarray: The computed histogram.
+
+        """
+        hist, _ = histogram(features, bins=self.bins, density=self.density)
         return hist
