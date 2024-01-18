912e5a6c0af4ba80faa615fb97cd0b2d611ccdca
Nicolas Depelteau <nicolas@depelteau.ca>
2024-01-17 19:34:24 -0500

    added environment variables

---
+++ TP1/src/utils/environment.py
@@ -0,0 +1,18 @@
+"""
+Environment configuration.
+"""
+from os import getenv
+from dotenv import load_dotenv
+
+
+class Environment:
+    """
+    Represents the environment configuration.
+
+    Attributes:
+        wanb_api_key (str): The API key for Weights & Biases.
+    """
+
+    def __init__(self):
+        load_dotenv(".env")
+        self.wanb_api_key = getenv("WANDB_API_KEY")
