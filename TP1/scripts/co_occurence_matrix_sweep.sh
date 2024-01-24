949388850408dbbd62065f4a9a136e4f9498a51c
Sebastien Goll <sebastiengoll@hotmail.fr>
2024-01-24 16:28:30 -0500

    Removed every from CM sweep
    Every is not supported for B&w images, if we want to use any, we should first find a way to handle those cases

--- TP1/scripts/co_occurence_matrix_sweep.sh
+++ TP1/scripts/co_occurence_matrix_sweep.sh
@@ -3,7 +3,7 @@ python main.py --multirun \
     feature_extractor=co_occurrence_matrix \
     feature_extractor.angles=0,0.785398163,1.57079633,2.35619449 \
     feature_extractor.levels=4,6,8 \
-    feature_extractor.channel=grey,red,green,blue,every \
+    feature_extractor.channel=grey,red,green,blue \
     descriptor=matrix_flattener
 
 
