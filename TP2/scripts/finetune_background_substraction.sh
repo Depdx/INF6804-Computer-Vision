f4662d8f9b4b44b36ffaeac06181817cd93327e3
Nicolas Depelteau <nicolas@depelteau.ca>
2024-02-15 15:25:15 -0500

    script de finetune du background subtraction

---
+++ TP2/scripts/finetune_background_substraction.sh
@@ -0,0 +1,4 @@
+python main.py --multirun \
+    segmentation_method=background_subtraction \
+    segmentation_method.threshold=40,50,60,70,80,90,100 \
+    batch_size=64 \
---
+++ TP2/scripts/run_background_substraction.sh
@@ -0,0 +1,4 @@
+python main.py \
+    segmentation_method=background_subtraction \
+    segmentation_method.threshold=55 \
+    batch_size=64
